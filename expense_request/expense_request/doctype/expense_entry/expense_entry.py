0d80cbc3a9fbc513ad2b2b9851f86d1e8cd4dd61
nyota <macharianyota@gmail.com>
2025-10-07 04:20:18 +0000

    update remark on submit

--- expense_request/expense_request/doctype/expense_entry/expense_entry.py
+++ expense_request/expense_request/doctype/expense_entry/expense_entry.py
@@ -22,12 +22,13 @@ class ExpenseEntry(Document):
                 rje.posting_date = frappe.utils.nowdate()
                 rje.submit()
 
-    def on_submit(self):
+    def before_save(self):
         # autofill remarks with expense type if empty
         if not self.remarks:
             self.remarks = ""
-            for entry in self.expenses:
-                self.remarks += entry.expense_account + ": " + str(entry.amount) + "\n"
-            
-            self.save()
+        
+        for entry in self.expenses:
+            self.remarks += entry.expense_account + ": " + str(entry.amount) + "\n"
+        
+        self.save()
             
